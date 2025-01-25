c2954805d3e8936150fe982c022b6f2f174c313d
Rivu <101360248+the-rivulet@users.noreply.github.com>
2025-01-25 22:04:45 +0000

    refactorization

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,14 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 let getId = function (x) { return document.getElementById(x); };
+let sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 ;
 const IMSIZE = 1.1;
 let noteTimes = [];
@@ -64,146 +74,152 @@ function hideKitties() {
     getId("topright").style.right = "-175px";
     getId("fish").style.top = "-300px";
 }
-function kittiesCloseUpClap(delay, keepKittiesShown = false) {
-    hideKitties();
-    // Now show them again
-    getId("topleft").style.top = "0%";
-    getId("topleft").style.left = "0%";
-    setTimeout(() => { getId("topright").style.top = "0%"; getId("topright").style.right = "0%"; }, delay * 3);
-    setTimeout(() => { getId("bottom").style.bottom = "0%"; }, delay * 6);
-    // Now clap!
-    setTimeout(() => {
-        getId("topleft").src = "assets/nova-topleft-clap1.png";
-        getId("topright").src = "assets/nova-topright-clap1.png";
-    }, delay * 10);
-    setTimeout(() => {
-        getId("topleft").src = "assets/nova-topleft-clap2.png";
-        getId("topright").src = "assets/nova-topright-clap2.png";
-    }, delay * 11);
-    setTimeout(() => {
-        getId("topleft").src = "assets/nova-topleft-clap1.png";
-        getId("topright").src = "assets/nova-topright-clap1.png";
-    }, delay * 12);
-    setTimeout(() => {
-        getId("topleft").src = "assets/nova-topleft-clap2.png";
-        getId("topright").src = "assets/nova-topright-clap2.png";
-    }, delay * 13);
-    setTimeout(() => {
-        getId("topleft").src = "assets/nova-topleft.png";
-        getId("topright").src = "assets/nova-topright.png";
-        getId("bottom").src = "assets/nova-bottom.png";
-        if (!keepKittiesShown)
-            hideKitties();
-    }, delay * 14);
-    // And add the notes...
-    for (let i of [10, 12]) {
-        noteTimes.push({ time: Date.now() + delay * i, key: "k", onHit(good) {
-                getId("bottom").src = "assets/nova-bottom-" + (good ? "clap1.png" : "miss.png");
-                if (good)
-                    setTimeout(() => { getId("bottom").src = "assets/nova-bottom-clap2.png"; }, delay);
-            } });
-    }
+function kittiesCloseUpClap(delay_1) {
+    return __awaiter(this, arguments, void 0, function* (delay, keepKittiesShown = false) {
+        hideKitties();
+        // Now show them again
+        let tl = getId("topleft"), tr = getId("topright"), b = getId("bottom");
+        tl.style.top = "0%";
+        tl.style.left = "0%";
+        // And add the notes...
+        for (let i of [10, 12]) {
+            noteTimes.push({ time: Date.now() + delay * i, key: "k", onHit(good) {
+                    b.src = "assets/nova-bottom-" + (good ? "clap1.png" : "miss.png");
+                    if (good)
+                        setTimeout(() => { b.src = "assets/nova-bottom-clap2.png"; }, delay);
+                } });
+        }
+        yield sleep(delay * 3).then(() => { tr.style.top = "0%"; tr.style.right = "0%"; })
+            .then(() => sleep(delay * 3)).then(() => { b.style.bottom = "0%"; })
+            .then(() => sleep(delay * 4)).then(() => {
+            tl.src = "assets/nova-topleft-clap1.png";
+            tr.src = "assets/nova-topright-clap1.png";
+        })
+            .then(() => sleep(delay * 1)).then(() => {
+            tl.src = "assets/nova-topleft-clap2.png";
+            tr.src = "assets/nova-topright-clap2.png";
+        })
+            .then(() => sleep(delay * 1)).then(() => {
+            tl.src = "assets/nova-topleft-clap1.png";
+            tr.src = "assets/nova-topright-clap1.png";
+        })
+            .then(() => sleep(delay * 1)).then(() => {
+            tl.src = "assets/nova-topleft-clap2.png";
+            tr.src = "assets/nova-topright-clap2.png";
+        })
+            .then(() => sleep(delay * 1)).then(() => {
+            tl.src = "assets/nova-topleft.png";
+            tr.src = "assets/nova-topright.png";
+            b.src = "assets/nova-bottom.png";
+            if (!keepKittiesShown)
+                hideKitties();
+        }).then(() => sleep(delay * 2));
+    });
 }
-function kittiesClap(delay, keepKittiesShown = false) {
-    hideKitties();
-    // Now show them again
-    getId("left").style.bottom = "0%";
-    setTimeout(() => { getId("middle").style.bottom = "0%"; }, delay * 3);
-    setTimeout(() => { getId("right").style.bottom = "0%"; }, delay * 6);
-    // Now clap!
-    setTimeout(() => {
-        getId("left").src = "assets/nova-left-clap1.png";
-        getId("middle").src = "assets/nova-middle-clap1.png";
-    }, delay * 10);
-    setTimeout(() => {
-        getId("left").src = "assets/nova-left-clap2.png";
-        getId("middle").src = "assets/nova-middle-clap2.png";
-    }, delay * 11);
-    setTimeout(() => {
-        getId("left").src = "assets/nova-left-clap1.png";
-        getId("middle").src = "assets/nova-middle-clap1.png";
-    }, delay * 12);
-    setTimeout(() => {
-        getId("left").src = "assets/nova-left-clap2.png";
-        getId("middle").src = "assets/nova-middle-clap2.png";
-    }, delay * 13);
-    setTimeout(() => {
-        getId("left").src = "assets/nova-left.png";
-        getId("middle").src = "assets/nova-middle.png";
-        getId("right").src = "assets/nova-right.png";
-        if (!keepKittiesShown)
-            hideKitties();
-    }, delay * 14);
-    // And add the notes...
-    for (let i of [10, 12]) {
-        noteTimes.push({ time: Date.now() + delay * i, key: "k", onHit(good) {
-                getId("right").src = "assets/nova-right-" + (good ? "clap1.png" : "miss.png");
-                if (good)
-                    setTimeout(() => { getId("right").src = "assets/nova-right-clap2.png"; }, delay);
-            } });
-    }
+function kittiesClap(delay_1) {
+    return __awaiter(this, arguments, void 0, function* (delay, keepKittiesShown = false) {
+        hideKitties();
+        // Now show them again
+        let l = getId("left"), m = getId("middle"), r = getId("right");
+        l.style.bottom = "0%";
+        // And add the notes...
+        for (let i of [10, 12]) {
+            noteTimes.push({ time: Date.now() + delay * i, key: "k", onHit(good) {
+                    r.src = "assets/nova-right-" + (good ? "clap1.png" : "miss.png");
+                    if (good)
+                        setTimeout(() => { r.src = "assets/nova-right-clap2.png"; }, delay);
+                } });
+        }
+        yield sleep(delay * 3).then(() => { m.style.bottom = "0%"; })
+            .then(() => sleep(delay * 3)).then(() => { r.style.bottom = "0%"; })
+            .then(() => sleep(delay * 4)).then(() => {
+            l.src = "assets/nova-left-clap1.png";
+            m.src = "assets/nova-middle-clap1.png";
+        })
+            .then(() => sleep(delay * 1)).then(() => {
+            l.src = "assets/nova-left-clap2.png";
+            m.src = "assets/nova-middle-clap2.png";
+        })
+            .then(() => sleep(delay * 1)).then(() => {
+            l.src = "assets/nova-left-clap1.png";
+            m.src = "assets/nova-middle-clap1.png";
+        })
+            .then(() => sleep(delay * 1)).then(() => {
+            l.src = "assets/nova-left-clap2.png";
+            m.src = "assets/nova-middle-clap2.png";
+        })
+            .then(() => sleep(delay * 1)).then(() => {
+            l.src = "assets/nova-left.png";
+            m.src = "assets/nova-middle.png";
+            r.src = "assets/nova-right.png";
+            if (!keepKittiesShown)
+                hideKitties();
+        }).then(() => sleep(delay * 2));
+    });
 }
-function kittiesSpin(delay, keepKittiesShown = false) {
-    // This will only work right after a "kittiesClap" (not CloseUp)
-    for (let time = 0; time < 4; time++) {
-        setTimeout(() => {
-            for (let i of ["left", "middle", "right"])
-                getId(i).style.height = "calc(" + (IMSIZE * 0.9) + " * 647px)";
-        }, delay * 2 * time);
-        setTimeout(() => {
-            for (let i of ["left", "middle", "right"])
+function kittiesSpin(delay_1) {
+    return __awaiter(this, arguments, void 0, function* (delay, keepKittiesShown = false, extraWait = 0) {
+        // This will only work right after a "kittiesClap" (not CloseUp)
+        let l = getId("left"), m = getId("middle"), r = getId("right");
+        noteTimes.push({ time: Date.now() + delay * 8, key: "j", onHit(good) {
+                r.style.height = "calc(" + (good ? IMSIZE / 2 : IMSIZE * 0.8) + " * 647px)";
+            } }, { time: Date.now() + delay * 11, key: "j", invert: true, onHit(good) {
+                r.src = "assets/nova-right-pose" + (good ? ".png" : "-miss.png");
+                r.style.height = "calc(" + IMSIZE + " * 647px)";
+                setTimeout(() => { r.src = "assets/nova-right.png"; }, delay * 3);
+            } });
+        for (let time = 0; time < 4; time++) {
+            yield sleep(extraWait).then(() => { for (let i of [l, m, r])
+                i.style.height = "calc(" + (IMSIZE * 0.9) + " * 647px)"; })
+                .then(() => sleep(delay * 1)).then(() => { for (let i of [l, m, r])
+                i.style.height = "calc(" + IMSIZE + " * 647px)"; })
+                .then(() => sleep(delay * 1));
+        }
+        yield sleep(0).then(() => { for (let i of [l, m])
+            i.style.height = "calc(" + (IMSIZE / 2) + " * 647px)"; })
+            .then(() => sleep(delay * 3)).then(() => {
+            for (let i of [l, m])
+                i.style.height = "calc(" + IMSIZE + " * 647px)";
+            l.src = "assets/nova-left-pose.png";
+            m.src = "assets/nova-middle-pose.png";
+        })
+            .then(() => sleep(delay * 3)).then(() => {
+            // in case the note was missed
+            r.style.height = "calc(" + IMSIZE + " * 647px)";
+            l.src = "assets/nova-left.png";
+            m.src = "assets/nova-middle.png";
+            r.src = "assets/nova-right.png";
+            if (!keepKittiesShown)
+                hideKitties();
+        });
+    });
+}
+function kittiesFish(delay_1) {
+    return __awaiter(this, arguments, void 0, function* (delay, keepKittiesShown = false) {
+        getId("fish").style.transition = (delay * 8 / 1000) + "s";
+        getId("fish").style.top = "0%";
+        noteTimes.push({ time: Date.now() + delay * 11, key: "k", value: 2, onHit(good) {
+                getId("right").style.height = "calc(" + IMSIZE + " * 647px)";
+                getId("right").src = "assets/nova-right-" + (good ? "fish.png" : "miss.png");
+            }, });
+        yield sleep(delay * 8).then(() => { getId("left").style.height = "calc(" + (IMSIZE / 2) + " * 647px)"; })
+            .then(() => sleep(delay * 1)).then(() => { getId("middle").style.height = "calc(" + (IMSIZE / 2) + " * 647px)"; })
+            .then(() => sleep(delay * 1)).then(() => { getId("right").style.height = "calc(" + (IMSIZE / 2) + " * 647px)"; })
+            .then(() => sleep(delay * 1)).then(() => {
+            for (let i of ["left", "middle"])
                 getId(i).style.height = "calc(" + IMSIZE + " * 647px)";
-        }, delay * (1 + 2 * time));
-    }
-    // Now do a BIG squish, then jump up!
-    setTimeout(() => {
-        for (let i of ["left", "middle"])
-            getId(i).style.height = "calc(0.65 * 647px)";
-    }, delay * 8);
-    noteTimes.push({ time: Date.now() + delay * 8, key: "j", onHit(good) {
-            getId("right").style.height = "calc(" + (good ? 0.65 : 0.9) + " * 647px)";
-        }, });
-    setTimeout(() => {
-        for (let i of ["left", "middle", "right"])
-            getId(i).style.height = "calc(" + IMSIZE + " * 647px)";
-        getId("left").src = "assets/nova-left-pose.png";
-        getId("middle").src = "assets/nova-middle-pose.png";
-    }, delay * 11);
-    noteTimes.push({ time: Date.now() + delay * 11, key: "j", invert: true, onHit(good) {
-            getId("right").src = "assets/nova-right-pose" + (good ? ".png" : "-miss.png");
-            setTimeout(() => { getId("right").src = "assets/nova-right.png"; }, delay * 3);
-        }, });
-    setTimeout(() => {
-        getId("left").src = "assets/nova-left.png";
-        getId("middle").src = "assets/nova-middle.png";
-        if (!keepKittiesShown)
-            hideKitties();
-    }, delay * 14);
-}
-function kittiesFish(delay, keepKittiesShown = false) {
-    getId("fish").style.transition = (delay * 8 / 1000) + "s";
-    getId("fish").style.top = "0%";
-    setTimeout(() => { getId("left").style.height = "calc(" + (IMSIZE / 2) + " * 647px)"; }, delay * 8);
-    setTimeout(() => { getId("middle").style.height = "calc(" + (IMSIZE / 2) + " * 647px)"; }, delay * 9);
-    setTimeout(() => { getId("right").style.height = "calc(" + (IMSIZE / 2) + " * 647px)"; }, delay * 10);
-    setTimeout(() => {
-        for (let i of ["left", "middle"])
-            getId(i).style.height = "calc(" + IMSIZE + " * 647px)";
-        getId("left").src = "assets/nova-left-fish.png";
-        getId("middle").src = "assets/nova-middle-fish.png";
-    }, delay * 11);
-    noteTimes.push({ time: Date.now() + delay * 11, key: "k", value: 2, onHit(good) {
+            getId("left").src = "assets/nova-left-fish.png";
+            getId("middle").src = "assets/nova-middle-fish.png";
+        })
+            .then(() => sleep(delay * 3)).then(() => {
+            getId("left").src = "assets/nova-left.png";
+            getId("middle").src = "assets/nova-middle.png";
             getId("right").style.height = "calc(" + IMSIZE + " * 647px)";
-            getId("right").src = "assets/nova-right-" + (good ? "fish.png" : "miss.png");
-        }, });
-    setTimeout(() => {
-        getId("left").src = "assets/nova-left.png";
-        getId("middle").src = "assets/nova-middle.png";
-        getId("right").style.height = "calc(" + IMSIZE + " * 647px)";
-        getId("right").src = "assets/nova-right.png";
-        if (!keepKittiesShown)
-            hideKitties();
-    }, delay * 14);
+            getId("right").src = "assets/nova-right.png";
+            if (!keepKittiesShown)
+                hideKitties();
+        });
+    });
 }
 let bestScores = {}, songsPlayed = [];
 function setUp() {
--- typescript/main.ts
+++ typescript/main.ts
@@ -1,4 +1,5 @@
 let getId = function<T extends HTMLElement>(x: string) { return document.getElementById(x) as T; }
+let sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 interface Note {time: number, key: string, invert?: boolean, value?: number, onHit: (good: boolean) => void};
 
@@ -64,137 +65,135 @@ function hideKitties() {
   getId("fish").style.top = "-300px";
 }
 
-function kittiesCloseUpClap(delay: number, keepKittiesShown = false) {
+async function kittiesCloseUpClap(delay: number, keepKittiesShown = false) {
   hideKitties();
   // Now show them again
-  getId("topleft").style.top = "0%"; getId("topleft").style.left = "0%";
-  setTimeout(() => { getId("topright").style.top = "0%"; getId("topright").style.right = "0%"; }, delay * 3);
-  setTimeout(() => { getId("bottom").style.bottom = "0%"; }, delay * 6);
-  // Now clap!
-  setTimeout(() => {
-    getId<HTMLImageElement>("topleft").src = "assets/nova-topleft-clap1.png";
-    getId<HTMLImageElement>("topright").src = "assets/nova-topright-clap1.png";
-  }, delay * 10);
-  setTimeout(() => {
-    getId<HTMLImageElement>("topleft").src = "assets/nova-topleft-clap2.png";
-    getId<HTMLImageElement>("topright").src = "assets/nova-topright-clap2.png";
-  }, delay * 11);
-  setTimeout(() => {
-    getId<HTMLImageElement>("topleft").src = "assets/nova-topleft-clap1.png";
-    getId<HTMLImageElement>("topright").src = "assets/nova-topright-clap1.png";
-  }, delay * 12);
-  setTimeout(() => {
-    getId<HTMLImageElement>("topleft").src = "assets/nova-topleft-clap2.png";
-    getId<HTMLImageElement>("topright").src = "assets/nova-topright-clap2.png";
-  }, delay * 13);
-  setTimeout(() => {
-    getId<HTMLImageElement>("topleft").src = "assets/nova-topleft.png";
-    getId<HTMLImageElement>("topright").src = "assets/nova-topright.png";
-    getId<HTMLImageElement>("bottom").src = "assets/nova-bottom.png";
-    if(!keepKittiesShown) hideKitties();
-  }, delay * 14);
+  let tl = getId<HTMLImageElement>("topleft"), tr = getId<HTMLImageElement>("topright"), b = getId<HTMLImageElement>("bottom");
+  tl.style.top = "0%"; tl.style.left = "0%";
   // And add the notes...
   for(let i of [10, 12]) {
     noteTimes.push({time: Date.now() + delay * i, key: "k", onHit(good) {
-      getId<HTMLImageElement>("bottom").src = "assets/nova-bottom-" + (good ? "clap1.png" : "miss.png");
-      if(good) setTimeout(() => { getId<HTMLImageElement>("bottom").src = "assets/nova-bottom-clap2.png"; }, delay);
+      b.src = "assets/nova-bottom-" + (good ? "clap1.png" : "miss.png");
+      if(good) setTimeout(() => { b.src = "assets/nova-bottom-clap2.png"; }, delay);
     }});
   }
+  await sleep(delay * 3).then(() => { tr.style.top = "0%"; tr.style.right = "0%"; })
+  .then(() => sleep(delay * 3)).then(() => { b.style.bottom = "0%"; })
+  .then(() => sleep(delay * 4)).then(() => {
+    tl.src = "assets/nova-topleft-clap1.png";
+    tr.src = "assets/nova-topright-clap1.png";
+  })
+  .then(() => sleep(delay * 1)).then(() => {
+    tl.src = "assets/nova-topleft-clap2.png";
+    tr.src = "assets/nova-topright-clap2.png";
+  })
+  .then(() => sleep(delay * 1)).then(() => {
+    tl.src = "assets/nova-topleft-clap1.png";
+    tr.src = "assets/nova-topright-clap1.png";
+  })
+  .then(() => sleep(delay * 1)).then(() => {
+    tl.src = "assets/nova-topleft-clap2.png";
+    tr.src = "assets/nova-topright-clap2.png";
+  })
+  .then(() => sleep(delay * 1)).then(() => {
+    tl.src = "assets/nova-topleft.png";
+    tr.src = "assets/nova-topright.png";
+    b.src = "assets/nova-bottom.png";
+    if(!keepKittiesShown) hideKitties();
+  }).then(() => sleep(delay * 2));
 }
 
-function kittiesClap(delay: number, keepKittiesShown = false) {
+async function kittiesClap(delay: number, keepKittiesShown = false) {
   hideKitties();
   // Now show them again
-  getId("left").style.bottom = "0%";
-  setTimeout(() => { getId("middle").style.bottom = "0%"; }, delay * 3);
-  setTimeout(() => { getId("right").style.bottom = "0%"; }, delay * 6);
-  // Now clap!
-  setTimeout(() => {
-    getId<HTMLImageElement>("left").src = "assets/nova-left-clap1.png";
-    getId<HTMLImageElement>("middle").src = "assets/nova-middle-clap1.png";
-  }, delay * 10);
-  setTimeout(() => {
-    getId<HTMLImageElement>("left").src = "assets/nova-left-clap2.png";
-    getId<HTMLImageElement>("middle").src = "assets/nova-middle-clap2.png";
-  }, delay * 11);
-  setTimeout(() => {
-    getId<HTMLImageElement>("left").src = "assets/nova-left-clap1.png";
-    getId<HTMLImageElement>("middle").src = "assets/nova-middle-clap1.png";
-  }, delay * 12);
-  setTimeout(() => {
-    getId<HTMLImageElement>("left").src = "assets/nova-left-clap2.png";
-    getId<HTMLImageElement>("middle").src = "assets/nova-middle-clap2.png";
-  }, delay * 13);
-  setTimeout(() => {
-    getId<HTMLImageElement>("left").src = "assets/nova-left.png";
-    getId<HTMLImageElement>("middle").src = "assets/nova-middle.png";
-    getId<HTMLImageElement>("right").src = "assets/nova-right.png";
-    if(!keepKittiesShown) hideKitties();
-  }, delay * 14);
+  let l = getId<HTMLImageElement>("left"), m = getId<HTMLImageElement>("middle"), r = getId<HTMLImageElement>("right");
+  l.style.bottom = "0%";
   // And add the notes...
   for(let i of [10, 12]) {
     noteTimes.push({time: Date.now() + delay * i, key: "k", onHit(good) {
-      getId<HTMLImageElement>("right").src = "assets/nova-right-" + (good ? "clap1.png" : "miss.png");
-      if(good) setTimeout(() => { getId<HTMLImageElement>("right").src = "assets/nova-right-clap2.png"; }, delay);
+      r.src = "assets/nova-right-" + (good ? "clap1.png" : "miss.png");
+      if(good) setTimeout(() => { r.src = "assets/nova-right-clap2.png"; }, delay);
     }});
   }
+  await sleep(delay * 3).then(() => { m.style.bottom = "0%"; })
+  .then(() => sleep(delay * 3)).then(() => { r.style.bottom = "0%"; })
+  .then(() => sleep(delay * 4)).then(() => {
+    l.src = "assets/nova-left-clap1.png";
+    m.src = "assets/nova-middle-clap1.png";
+  })
+  .then(() => sleep(delay * 1)).then(() => {
+    l.src = "assets/nova-left-clap2.png";
+    m.src = "assets/nova-middle-clap2.png";
+  })
+  .then(() => sleep(delay * 1)).then(() => {
+    l.src = "assets/nova-left-clap1.png";
+    m.src = "assets/nova-middle-clap1.png";
+  })
+  .then(() => sleep(delay * 1)).then(() => {
+    l.src = "assets/nova-left-clap2.png";
+    m.src = "assets/nova-middle-clap2.png";
+  })
+  .then(() => sleep(delay * 1)).then(() => {
+    l.src = "assets/nova-left.png";
+    m.src = "assets/nova-middle.png";
+    r.src = "assets/nova-right.png";
+    if(!keepKittiesShown) hideKitties();
+  }).then(() => sleep(delay * 2));
 }
 
-function kittiesSpin(delay: number, keepKittiesShown = false) {
+async function kittiesSpin(delay: number, keepKittiesShown = false, extraWait = 0) {
   // This will only work right after a "kittiesClap" (not CloseUp)
-  for(let time = 0; time < 4; time++) {
-    setTimeout(() => {
-      for(let i of ["left", "middle", "right"]) getId(i).style.height = "calc(" + (IMSIZE*0.9) + " * 647px)";
-    }, delay * 2 * time);
-    setTimeout(() => {
-      for(let i of ["left", "middle", "right"]) getId(i).style.height = "calc(" + IMSIZE + " * 647px)";
-    }, delay * (1 + 2 * time));
-  }
-  // Now do a BIG squish, then jump up!
-  setTimeout(() => {
-    for(let i of ["left", "middle"]) getId(i).style.height = "calc(0.65 * 647px)";
-  }, delay * 8);
+  let l = getId<HTMLImageElement>("left"), m = getId<HTMLImageElement>("middle"), r = getId<HTMLImageElement>("right");
   noteTimes.push({time: Date.now() + delay * 8, key: "j", onHit(good) {
-    getId("right").style.height = "calc(" + (good ? 0.65 : 0.9) + " * 647px)";
-  },});
-  setTimeout(() => {
-    for(let i of ["left", "middle", "right"]) getId(i).style.height = "calc(" + IMSIZE + " * 647px)";
-    getId<HTMLImageElement>("left").src = "assets/nova-left-pose.png";
-    getId<HTMLImageElement>("middle").src = "assets/nova-middle-pose.png";
-  }, delay * 11);
-  noteTimes.push({time: Date.now() + delay * 11, key: "j", invert: true, onHit(good) {
-    getId<HTMLImageElement>("right").src = "assets/nova-right-pose" + (good ? ".png" : "-miss.png");
-    setTimeout(() => { getId<HTMLImageElement>("right").src = "assets/nova-right.png"; }, delay * 3);
-  },});
-  setTimeout(() => {
-    getId<HTMLImageElement>("left").src = "assets/nova-left.png";
-    getId<HTMLImageElement>("middle").src = "assets/nova-middle.png";
+    r.style.height = "calc(" + (good ? IMSIZE/2 : IMSIZE*0.8) + " * 647px)";
+  }}, {time: Date.now() + delay * 11, key: "j", invert: true, onHit(good) {
+    r.src = "assets/nova-right-pose" + (good ? ".png" : "-miss.png");
+    r.style.height = "calc(" + IMSIZE + " * 647px)";
+    setTimeout(() => { r.src = "assets/nova-right.png"; }, delay * 3);
+  }});
+  for(let time = 0; time < 4; time++) {
+    await sleep(extraWait).then(() => { for(let i of [l, m, r]) i.style.height = "calc(" + (IMSIZE*0.9) + " * 647px)"; })
+    .then(() => sleep(delay * 1)).then(() => { for(let i of [l, m, r]) i.style.height = "calc(" + IMSIZE + " * 647px)"; })
+    .then(() => sleep(delay * 1));
+  }
+  await sleep(0).then(() => { for(let i of [l, m]) i.style.height = "calc(" + (IMSIZE/2) + " * 647px)"; })
+  .then(() => sleep(delay * 3)).then(() => {
+    for(let i of [l, m]) i.style.height = "calc(" + IMSIZE + " * 647px)";
+    l.src = "assets/nova-left-pose.png";
+    m.src = "assets/nova-middle-pose.png";
+  })
+  .then(() => sleep(delay * 3)).then(() => {
+    // in case the note was missed
+    r.style.height = "calc(" + IMSIZE + " * 647px)";
+    l.src = "assets/nova-left.png";
+    m.src = "assets/nova-middle.png";
+    r.src = "assets/nova-right.png";
     if(!keepKittiesShown) hideKitties();
-  }, delay * 14);
+  })
 }
 
-function kittiesFish(delay: number, keepKittiesShown = false) {
+async function kittiesFish(delay: number, keepKittiesShown = false) {
   getId("fish").style.transition = (delay * 8 / 1000) + "s";
   getId("fish").style.top = "0%";
-  setTimeout(() => { getId("left").style.height = "calc(" + (IMSIZE/2) + " * 647px)"; }, delay * 8);
-  setTimeout(() => { getId("middle").style.height = "calc(" + (IMSIZE/2) + " * 647px)"; }, delay * 9);
-  setTimeout(() => { getId("right").style.height = "calc(" + (IMSIZE/2) + " * 647px)"; }, delay * 10);
-  setTimeout(() => {
-    for(let i of ["left", "middle"]) getId(i).style.height = "calc(" + IMSIZE + " * 647px)";
-    getId<HTMLImageElement>("left").src = "assets/nova-left-fish.png";
-    getId<HTMLImageElement>("middle").src = "assets/nova-middle-fish.png";
-  }, delay * 11);
   noteTimes.push({time: Date.now() + delay * 11, key: "k", value: 2, onHit(good) {
     getId("right").style.height = "calc(" + IMSIZE + " * 647px)";
     getId<HTMLImageElement>("right").src = "assets/nova-right-" + (good ? "fish.png" : "miss.png");
   },});
-  setTimeout(() => {
+  await sleep(delay * 8).then(() => { getId("left").style.height = "calc(" + (IMSIZE/2) + " * 647px)"; })
+  .then(() => sleep(delay * 1)).then(() => { getId("middle").style.height = "calc(" + (IMSIZE/2) + " * 647px)"; })
+  .then(() => sleep(delay * 1)).then(() => { getId("right").style.height = "calc(" + (IMSIZE/2) + " * 647px)"; })
+  .then(() => sleep(delay * 1)).then(() => {
+    for(let i of ["left", "middle"]) getId(i).style.height = "calc(" + IMSIZE + " * 647px)";
+    getId<HTMLImageElement>("left").src = "assets/nova-left-fish.png";
+    getId<HTMLImageElement>("middle").src = "assets/nova-middle-fish.png";
+  })
+  .then(() => sleep(delay * 3)).then(() => {
     getId<HTMLImageElement>("left").src = "assets/nova-left.png";
     getId<HTMLImageElement>("middle").src = "assets/nova-middle.png";
     getId("right").style.height = "calc(" + IMSIZE + " * 647px)";
     getId<HTMLImageElement>("right").src = "assets/nova-right.png";
     if(!keepKittiesShown) hideKitties();
-  }, delay * 14);
+  });
 }
 
 let bestScores = {}, songsPlayed = [];
